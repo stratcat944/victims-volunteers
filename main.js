e6910223a3ca5a4342abf5b696e2b3e9ee71005b
Steve Domenico <stratcat944@gmail.com>
2015-06-23 14:34:44 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// Prompt user to enter the number of victims.
+var vicNumber = prompt("Please enter the number of victims you wish to help.") {
+	for(i = 0; i < vicNumber; 1++);
+}
+var vicName = prompt("Please enter the victim's name.")
+
+var vicNumber = ("Please enter the victim's number.")
+
+var vicStreet = ("Please enter the victim's street.")
+
+// Prompt user to enter number of volunteers.
+var volNumber = prompt("Please enter the number of volunteers you wish to enlist.");
+
