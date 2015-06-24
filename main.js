43295310c40443128ebc882c059067f361b0f539
Steve Domenico <stratcat944@gmail.com>
2015-06-24 15:16:13 -0600

    finished

--- main.js
+++ main.js
@@ -2,31 +2,50 @@
 
 // Initialize arrays
 
-var vicName = []
-
-var vicPhone = []
-
-var vicStreet = []
-
-var volName = []
-
-var volPhone = []
-
-var volStreet = []
+var victimName = [];
+var victimPhone = [];
+var victimStreet = [];
 
 
-var vicNumber = prompt ("Enter the number of victims you would like to help.");
+var volunteerName = [];
+var volunteerPhone = [];
+var volunteerStreet = [];
 
-for (i = vicNumber ; i < vicNumber ; i++ ) {
-	vicName.push(prompt("Enter the victim's name:"));
-	vicPhone.push(prompt("Enter the victim's phone number;"));
-	vicStreet.push(prompt("Enter the victim's street address:"));
+var victimCount = prompt ("Enter the number of victims you would like to help.");
+
+for (i = victimCount ; i < victimCount ; i++ ) {
+	victimName.push(prompt("Enter the victim's name:"));
+	victimPhone.push(prompt("Enter the victim's phone number;"));
+	victimStreet.push(prompt("Enter the victim's street address:"));
 }
 
-var volNumber = prompt ("Enter the number of volunteers you would like to enlist:");
+var volunteerCount = prompt ("Enter the number of volunteers you would like to enlist:");
 
-for ( i = volNumber ; i < volNumber ; i++ ) {
-	volName.push(prompt("Enter the volunteer's name:"));
-	volPhone.push(prompt("Enter the volunteer's phone number:"));
-	volStreet.push(prompt("Enter the volunteer's street address:"));
+for ( i = volunteerCount ; i < volunteerCount ; i++ ) {
+	volunteerName.push(prompt("Enter the volunteer's name:"));
+	volunteerPhone.push(prompt("Enter the volunteer's phone number:"));
+	volunteerStreet.push(prompt("Enter the volunteer's street address:"));
 }
+
+var output = "Victims: " + victimCount + "\n" + " Volunteers: " + volunteerCount + "\n";
+
+output += "____Victims:____\n";
+
+
+for(var i=0; i<victimCount; i++) {
+	output += "  Name: " + victimName[i] + ", " + "Phone: " + victimPhone[i] + ", " + "Street: " + victimStreet[i] + "\n";
+}
+
+output += "____Volunteers:____\n";
+
+for(var i=0; i<volunteerCount; i++) {
+	output += "  Name: " + volunteerName[i] + ", " + "Phone: " + volunteerPhone[i] + ", " + "Street: " + volunteerStreet[i] + "\n";
+}
+
+
+// Output
+alert(output);
+console.log(output);
+
+
+
